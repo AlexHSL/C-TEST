0ec8079f76e2b0a1c15413c01763c559e4975819
AlexHSL <2825883930@qq.com>
2019-09-13 10:04:55 +0800

    a test for strcat_s in exercise5

--- exercise5/exercise5/exercise5.cpp
+++ exercise5/exercise5/exercise5.cpp
@@ -7,17 +7,18 @@ using namespace std;
 
 int main()
 {
-	const char* s1 = "Hello ";
-	const char* s2 = "123";
-	char a[20];
-	strcpy_s(a, s1);
-	cout << (strcmp(a, s1) == 0 ? "" : "not ") << "equal\n";
-	cout << strcat_s(a, s2) << endl;
-	cout << a << endl;
-	cout << _strrev(a) << endl;
-	cout << _strset_s(a, 'c') << endl;
-	cout << a << endl;
-	cout << (strstr(s1, "ell") ? "" : "not ") << "found\n";
-	cout << (strchr(s1, 'c') ? "" : "not ") << "found\n";
+    const char* s1 = "Hello ";
+    const char* s2 = "123";
+    char a[20];
+    strcpy_s( a, s1 );
+	cout << strcat_s(a, 2 * strlen(s1) + 1, s1) << endl;
+    cout << ( strcmp( a, s1 ) == 0 ? "" : "not " ) << "equal\n";
+    cout << strcat_s( a, s2 ) << endl;
+    cout << a << endl;
+    cout << _strrev( a ) << endl;
+    cout << _strset_s( a, 'c' ) << endl;
+    cout << a << endl;
+    cout << ( strstr( s1, "ell" ) ? "" : "not " ) << "found\n";
+    cout << ( strchr( s1, 'c' ) ? "" : "not " ) << "found\n";
 
 }
