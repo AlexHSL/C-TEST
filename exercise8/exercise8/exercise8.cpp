cdb64c147aa773aac6ef70e8027bd9c3288a277f
AlexHSL <2825883930@qq.com>
2019-09-17 17:12:42 +0800

    exercise8 commited:my test for func

--- exercise8/exercise8/exercise8.cpp
+++ exercise8/exercise8/exercise8.cpp
@@ -1,21 +1,34 @@
-﻿// exercise8.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include "pch.h"
+#include <iostream>_
 
-#include "pch.h"
-#include <iostream>
-
+void g(int& i);
+void f(int& i);
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	//using namespace std;
+	////对指针的熟悉
+	//int a = 1;
+	//int *t=&a;
+	//*t = a;
+	//cout << (*t += 1) << " " << a << endl;
+	////
+	int count = 1;
+	g(count);
+	f(count);
+}
+void g(int& i)
+{
+	cout << "call g()" << endl;
+	if (i > 100) return;
+	i++;
+	f(i);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+void f(int& i)
+{
+	cout << "call f()" << endl;
+	if (i > 100) return;
+	i++;
+	g(i);
+}
