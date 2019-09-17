3b1e79dace872f2ed5281e1d13d01bd40628cae2
AlexHSL <2825883930@qq.com>
2019-09-17 16:11:27 +0800

    exercise7提交，没什么新东西

---
+++ exercise7/exercise7/exercise7.cpp
@@ -0,0 +1,20 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+//因为高中同学提到*&的问题，就看了一下，本来以为会很好玩，结果发现其实没什么，应该是他理解错了
+int main()
+{
+    using namespace std;
+    int temp = 1;
+    int temp2 = *&temp;
+	int x = temp;
+    cout << temp << endl;
+	cout << temp2 << endl;
+	cout << x << endl;
+    cout << ( temp = temp + 1 ) << temp2 << endl;
+	cout << temp << endl;
+	cout << temp2 << endl;
+	cout << x << endl;
+
+}
