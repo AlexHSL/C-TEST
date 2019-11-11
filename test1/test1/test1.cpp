46415fec7d4fce193a2d936307294798f52ddfb8
AlexHSL <2825883930@qq.com>
2019-11-11 20:49:12 +0800

    the class taught by teacher Pan started, so do some exercise

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+
+std :: vector<int> add( const std::vector<int> &a, const std::vector<int> &b )
+{
+    std::vector<int> res( a );
+
+    for(unsigned int i = 0; i < a.size(); i++ )
+    {
+        res[i] += b[i];
+    }
+
+    return res;
+}
+int main()
+{
+    std::vector<int> a = { 1, 2, 3 };
+    std::vector<int>res = add( a, a );
+	std::cout << res[0];
+}
+
+
+
